75bd1b3733525dbc5fb949e86e531b6ace968adb
vans <vans@test.com>
2016-11-12 16:20:35 +0100

    Add debug utilities

---
+++ server/src/debug/debugUtils.ts
@@ -0,0 +1,52 @@
+'use strict';
+
+import { IConnection } from 'vscode-languageserver';
+
+/**
+ * Tools for debugging logs
+ */
+export class DebugUtils {
+    private static _instance : DebugUtils
+
+    /**
+     * Get the singleton
+     */
+    public static get instance() : DebugUtils {
+        return DebugUtils._instance;
+    }
+
+    /**
+     * Initializes the debug environment
+     */
+    public static init(isDebugOn : boolean, connection : IConnection) {
+        if (DebugUtils._instance == null) {
+            DebugUtils._instance = new DebugUtils(isDebugOn, connection);
+        }
+    }
+
+    private isDebugOn : boolean;
+    private connection : IConnection;
+
+    private constructor(isDebugOn : boolean, connection : IConnection) {
+        this.isDebugOn = isDebugOn;
+        this.connection = connection;
+    }
+
+    /**
+     * Does a connection.console.log call if debug mode is activated
+     */
+    public connectionLog(text : string) {
+        if (this.isDebugOn) {
+            this.connection.console.log(text);
+        }
+    }
+
+    /**
+     * Does a console.log call if debug mode is activated
+     */
+    public log(text : string) {
+        if (this.isDebugOn) {
+            console.log(text);
+        }
+    }
+}
